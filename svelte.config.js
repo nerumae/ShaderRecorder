3747bbaaa9f13756c1953d864c9df2badd3edf02
nerumae <nerumae3@gmail.com>
2024-05-15 17:13:48 +0900

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
       //   fallback: "404.html",
     }),
     paths: {
-      base: process.argv.includes("dev") ? "" : `${"Shaderrender"}`,
+      base: process.argv.includes("dev") ? "" : `${"ShaderRender"}`,
     },
   },
 };
